7fb25a89f09cb882bb005b602b4078aee58153ae
MohamedGomaa <mohamedgomaa23722@gmail.com>
2023-09-19 02:16:03 +0300

    add new post method

--- src/main/java/com/example/jenkins/controller.java
+++ src/main/java/com/example/jenkins/controller.java
@@ -2,6 +2,7 @@ package com.example.jenkins;
 
 import lombok.Getter;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/users")
@@ -10,5 +11,10 @@ public class controller {
     @GetMapping
     public String getUser(){
         return "Gomaa";
+    }
+
+    @PostMapping
+    public String postUser() {
+        return "Mohamed gomaa posted";
     }
 }
