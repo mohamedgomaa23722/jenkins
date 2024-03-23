221efeb2f1c4605c2832b66c133fdb2a05d47821
Mohamed Gomaa <62193989+mohamedgomaa23722@users.noreply.github.com>
2024-03-23 07:00:58 +0200

    Update JenkinsApplication.java

--- src/main/java/com/example/jenkins/JenkinsApplication.java
+++ src/main/java/com/example/jenkins/JenkinsApplication.java
@@ -9,7 +9,7 @@ public class JenkinsApplication {
 	public static void main(String[] args) {
 		//SpringApplication.run(JenkinsApplication.class, args);
 		System.out.println("-------------TEST Jenkins-----------------");
-				System.out.println("-------------TEST Jenkins CHANGE-----------------");
+		System.out.println("-------------TEST Jenkins CHANGE-----------------");
 
 	}
 
